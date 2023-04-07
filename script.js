5e2a36785db69b9c01c8c042fbe66b0f40b58b98
darkGhost3142 <ghosttsushima3142@gmail.com>
2023-04-07 21:19:40 +0630

    javascript program was deleted

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-//Go to other html links
-/* const viewMoreButtons = document.querySelectorAll('.card a');
 
-for(let btn of viewMoreButtons) {
-    btn.addEventListener('click', (event) => {
-        let foodDrinkName = event.target.parentElement.querySelector('h5').innerText.toLowerCase();
-        window.location.href = `${foodDrinkName}.html`;
-    });
-} */
