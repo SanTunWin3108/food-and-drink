c60cb87b5020f5acbebd737e6c596ed3d529ed58
darkGhost3142 <ghosttsushima3142@gmail.com>
2023-04-07 22:25:15 +0630

    javascript program: get add to cart buttons,  get item details, make createObject function using item details, add itemObject to array, add array to localstorage and get array from localstorage

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
+const addToCartButtons = document.querySelectorAll('.card-body button');
+const itemArr = JSON.parse(localStorage.getItem('itemArr')) || [];
 
+for(let addBtn of addToCartButtons) {
+    addBtn.addEventListener('click', (e) => {
+        const addBtnParent = e.target.parentElement.parentElement;
+        const itemImage = addBtnParent.querySelector('img').src;
+        const itemName = addBtnParent.querySelector('h5').innerText;
+        const itemPrice = addBtnParent.querySelector('p').innerText;
+
+        //create item object
+        const itemObj = createObject(itemImage, itemName, itemPrice);
+        
+        //add item object to array
+        itemArr.push(itemObj);
+
+        //add array to localstorage
+        setToLocalstorage(itemArr);
+    });
+}
+
+const createObject = (itemImage, itemName, itemPrice) => {
+    return {
+        itemImage,
+        itemName,
+        itemPrice
+    };
+}
+
+const setToLocalstorage = (itemArr) => {
+    localStorage.setItem('itemArr', JSON.stringify(itemArr));
+}
